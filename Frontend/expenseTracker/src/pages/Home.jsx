923bdb6e2f1145ecb18612443442e656e80a6311
PatelSnehV <snehpatel1233@gmail.com>
2025-07-18 09:57:11 +0530

    .

--- Frontend/expenseTracker/src/pages/Home.jsx
+++ Frontend/expenseTracker/src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
     <div className="flex justify-center items-center h-screen">
       <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-b-4 border-purple-600"></div>
     </div>
-      ;
+      
     setLoading(true);
     try {
       const data = await axios.get("https://expense-tracker-backend-jkhf.onrender.com/", {
